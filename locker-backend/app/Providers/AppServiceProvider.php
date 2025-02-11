75137afad1d51435a9489c53f7946feed37f1fce
Merlin Hofmann | Merona <merlin@merona.de>
2025-02-11 20:11:55 +0000

    adds bearer auth to documentation

--- locker-backend/app/Providers/AppServiceProvider.php
+++ locker-backend/app/Providers/AppServiceProvider.php
@@ -3,28 +3,32 @@
 namespace App\Providers;
 
 use Carbon\CarbonImmutable;
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
 use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
+    public function register(): void {
         //
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        SecurityScheme::http('bearer', 'JWT');
+    public function boot(): void {
+
+        Scramble::afterOpenApiGenerated( function ( OpenApi $openApi ) {
+            $openApi->secure(
+                SecurityScheme::http( 'bearer', 'JWT' )
+            );
+        } );
 
         // Use CarbonImmutable for all date instances. Prevents date mutability.
-        Date::use(CarbonImmutable::class);
+        Date::use( CarbonImmutable::class );
     }
 }
