bd3655a4883b16de9fa35ac3e49a270c010576b0
Merlin Hofmann | Merona <merlin@merona.de>
2025-02-13 21:58:29 +0000

    adds TokenResponseResource.php

---
+++ locker-backend/app/Http/Resources/TokenResponseResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+
+
+use App\Models\User;
+use Dedoc\Scramble\Attributes\SchemaName;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property User $resource
+ */
+#[SchemaName('TokenResponse')]
+class TokenResponseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "token" => $this->resource->createToken( 'auth_token' )->plainTextToken,
+            "name" => $this->resource->name
+        ];
+    }
+}
