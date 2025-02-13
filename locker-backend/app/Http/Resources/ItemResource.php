dc9c60ecb1b96f9bf7455e7df5bd8cf325a5f184
Merlin Hofmann | Merona <merlin@merona.de>
2025-02-13 22:00:01 +0000

    adds properties to ItemResource.php and renames the api schema name

--- locker-backend/app/Http/Resources/ItemResource.php
+++ locker-backend/app/Http/Resources/ItemResource.php
@@ -3,12 +3,14 @@
 namespace App\Http\Resources;
 
 use App\Models\Item;
+use Dedoc\Scramble\Attributes\SchemaName;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
  * @property Item $resource
  */
+#[SchemaName('Item')]
 class ItemResource extends JsonResource {
     /**
      * Transform the resource into an array.
@@ -16,10 +18,13 @@ class ItemResource extends JsonResource {
      * @return array<string, mixed>
      */
     public function toArray( Request $request ): array {
-        return [ "id"          => $this->resource->id,
-                 "name"        => $this->resource->name,
-                 "description" => $this->resource->description,
-                 "image_path"  => $this->resource->image_path
+        return [
+            "id"          => $this->resource->id,
+            "name"        => $this->resource->name,
+            "description" => $this->resource->description,
+            "image_path"  => $this->resource->image_path,
+            "created_at"  => $this->resource->created_at,
+            "updated_at"  => $this->resource->updated_at
         ];
     }
 }
