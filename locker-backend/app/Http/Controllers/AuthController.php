5352825cd5d2195cdd17204317572b6281c7cbb5
Merlin Hofmann | Merona <merlin@merona.de>
2025-02-13 19:29:00 +0000

    adds explicit json object as return for user route

--- locker-backend/app/Http/Controllers/AuthController.php
+++ locker-backend/app/Http/Controllers/AuthController.php
@@ -10,36 +10,34 @@ use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-class AuthController extends Controller
-{
+class AuthController extends Controller {
     /**
      * Register
      *
      * @throws \Exception
      */
-    public function register(Request $request): JsonResponse
-    {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => [
+    public function register( Request $request ): JsonResponse {
+        $request->validate( [
+            'name'     => [ 'required', 'string', 'max:255' ],
+            'email'    => [
                 'required',
                 'string',
                 'email',
                 'max:255',
-                Rule::unique(User::class),
+                Rule::unique( User::class ),
             ],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
+            'password' => [ 'required', 'string', 'min:8', 'confirmed' ],
+        ] );
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        $user = User::create( [
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make( $request->password ),
+        ] );
 
-        $token = $user->createToken('auth_token')->plainTextToken;
+        $token = $user->createToken( 'auth_token' )->plainTextToken;
 
-        return response()->json(['token' => $token, 'name' => $user->name]);
+        return response()->json( [ 'token' => $token, 'name' => $user->name ] );
     }
 
     /**
@@ -47,40 +45,47 @@ class AuthController extends Controller
      *
      * @unauthenticated
      */
-    public function login(Request $request): JsonResponse
-    {
-        $request->validate([
-            'email' => 'required|email',
+    public function login( Request $request ): JsonResponse {
+        $request->validate( [
+            'email'    => 'required|email',
             'password' => 'required',
-        ]);
+        ] );
 
-        if (! Auth::attempt($request->only('email', 'password'))) {
-            throw ValidationException::withMessages(['email' => ['The provided credentials are incorrect.']]);
+        if ( ! Auth::attempt( $request->only( 'email', 'password' ) ) ) {
+            throw ValidationException::withMessages( [ 'email' => [ 'The provided credentials are incorrect.' ] ] );
         }
 
-        $token = $request->user()->createToken('auth_token')->plainTextToken;
+        $token = $request->user()->createToken( 'auth_token' )->plainTextToken;
 
-        return response()->json(['token' => $token, 'name' => $request->user()->name]);
+        return response()->json( [ 'token' => $token, 'name' => $request->user()->name ] );
 
     }
 
     /**
      * Logout
      */
-    public function logout(Request $request): JsonResponse
-    {
+    public function logout( Request $request ): JsonResponse {
         $request->user()->currentAccessToken()->delete();
 
-        return response()->json([
+        return response()->json( [
             'message' => 'Logged out successfully',
-        ]);
+        ] );
     }
 
     /**
      * Get current User
      */
-    public function user(Request $request): JsonResponse
-    {
-        return response()->json($request->user());
+    public function user( Request $request ): JsonResponse {
+
+        $user = $request->user();
+
+        return response()->json( [
+            "id"                => $user->id,
+            'name'              => $user->name,
+            'email'             => $user->email,
+            "email_verified_at" => $user->email_verified_at,
+            "created_at"        => $user->created_at,
+            "updated_at"        => $user->updated_at,
+        ] );
     }
 }
