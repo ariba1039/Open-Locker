f841f95bb291407cce5fc08959e6308c9a10a8cb
Merlin Hofmann | Merona <merlin@merona.de>
2025-02-13 19:28:15 +0000

    unify the style of route registration

--- locker-backend/routes/api.php
+++ locker-backend/routes/api.php
@@ -1,15 +1,14 @@
-
 <?php
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-});
+Route::controller( AuthController::class )->group( function () {
+    Route::post( 'login', 'login' );
+} );
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [AuthController::class, 'user']);
-});
+Route::middleware( 'auth:sanctum' )->controller( AuthController::class )->group( function () {
+    Route::post( 'register', 'register' );
+    Route::post( 'logout', 'logout' );
+    Route::get( 'user', 'user' );
+} );
