870d19d12a92b562c74840c5e7c1206dcadecf6e
Merlin Hofmann | Merona <merlin@merona.de>
2025-02-13 21:32:11 +0000

    add ItemController routes for item retrieval

--- locker-backend/routes/api.php
+++ locker-backend/routes/api.php
@@ -1,14 +1,22 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller( AuthController::class )->group( function () {
     Route::post( 'login', 'login' );
 } );
 
-Route::middleware( 'auth:sanctum' )->controller( AuthController::class )->group( function () {
-    Route::post( 'register', 'register' );
-    Route::post( 'logout', 'logout' );
-    Route::get( 'user', 'user' );
+Route::middleware( 'auth:sanctum' )->group( function () {
+
+    Route::controller( AuthController::class )->group( function () {
+        Route::post( 'register', 'register' );
+        Route::post( 'logout', 'logout' );
+        Route::get( 'user', 'user' );
+    } );
+    Route::controller( ItemController::class )->prefix( '/items' )->group( function () {
+        Route::get( '', 'index' );
+    } );
 } );
+
