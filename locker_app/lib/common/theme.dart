4ab881b54013815693e9e35d774a0e3bfcac829e
Simon Engelhardt <smn@nglhrdt.de>
2025-02-18 20:22:11 +0100

    update app theme to use black text color for displayLarge

--- locker_app/lib/common/theme.dart
+++ locker_app/lib/common/theme.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 final appTheme = ThemeData(
   colorSchemeSeed: Colors.green,
-  primaryColor: Colors.green,
   textTheme: const TextTheme(
     displayLarge: TextStyle(
       fontSize: 24,
+      color: Colors.black,
     ),
   ),
 );
