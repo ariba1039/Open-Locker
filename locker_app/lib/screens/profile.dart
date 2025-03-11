e17b77fe5ea11b5abc5e012b93a15ee16d88ffc1
Merlin Hofmann | Merona <merlin@merona.de>
2025-03-11 19:32:39 +0000

    add logout button to profil page

--- locker_app/lib/screens/profile.dart
+++ locker_app/lib/screens/profile.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:locker_app/screens/login.dart';
 import 'package:locker_app/services/user_service.dart';
-import 'package:locker_app/widgets/side_nav.dart';
 import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -12,6 +11,23 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final name = context.watch<UserService>().user;
-    return Center(child: Text(name));
+    return Scaffold(
+      body: Center(
+        child: Text(name),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 16.0),
+        child: TextButton.icon(
+          onPressed: () async {
+            await context.read<UserService>().logout();
+            if (context.mounted) {
+              context.go(LoginScreen.route);
+            }
+          },
+          icon: const Icon(Icons.logout),
+          label: const Text('Logout'),
+        ),
+      ),
+    );
   }
 }
